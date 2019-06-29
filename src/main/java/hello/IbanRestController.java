425efdeafb7674effc2545e083a5996126c2b8c2
Gediminas <gedgeds@gmail.com>
2019-06-29 22:41:43 +0300

    Update and rename Rest Controller class

--- src/main/java/hello/IbanRestController.java
+++ src/main/java/hello/IbanRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class GreetingController {
+public class IbanRestController {
 
     @RequestMapping(value = "/{ibans}", method = RequestMethod.GET)
     public boolean checkIban(@PathVariable("ibans") String iban) {
